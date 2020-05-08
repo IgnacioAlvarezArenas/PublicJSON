88b04eeaff59940fd7211d6648dcd66ebdec6ff7
Ignacio Alvarez <ignalvarezarenas@gmail.com>
2020-05-08 22:22:40 +0100

    timer

--- Compute-LAB2-NonBoot.ps1
+++ Compute-LAB2-NonBoot.ps1
@@ -1,2 +1,3 @@
-﻿Set-ItemProperty -Path "HKLM:\System\ControlSet001\Services\volmgr" -name Start -Value 4
+﻿sleep 300
+Set-ItemProperty -Path "HKLM:\System\ControlSet001\Services\volmgr" -name Start -Value 4
 shutdown /r /t 30 /f
